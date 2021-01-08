1a63422460727f04f9f2b69562785538cb3b754e
sebastiankut <sebastian_kut@yahoo.co.uk>
2021-01-08 08:51:14 +0100

    products added

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Home.css';
 import Product from './Product';
+import { products } from './products';
 
 function Home() {
   return (
@@ -11,12 +12,49 @@ function Home() {
           src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2021/NYNY/Fuji_TallHero_NYNY_en_US_2x._CB412256578_.jpg"
           alt=""
         />
+        <div className="home__row">
+          {products.slice(0, 2).map((product) => {
+            const { id, title, rating, price, image } = product;
+            return (
+              <Product
+                key={id}
+                title={title}
+                price={price}
+                image={image}
+                rating={rating}
+              />
+            );
+          })}
+        </div>
+        <div className="home__row">
+          {products.slice(2, 5).map((product) => {
+            const { id, title, rating, price, image } = product;
+            return (
+              <Product
+                key={id}
+                title={title}
+                price={price}
+                image={image}
+                rating={rating}
+              />
+            );
+          })}
+        </div>
+        <div className="home__row">
+          {products.slice(5).map((product) => {
+            const { id, title, rating, price, image } = product;
+            return (
+              <Product
+                key={id}
+                title={title}
+                price={price}
+                image={image}
+                rating={rating}
+              />
+            );
+          })}
+        </div>
       </div>
-      <div className="home__row">
-        <Product />
-      </div>
-      <div className="home__row"></div>
-      <div className="home__row"></div>
     </div>
   );
 }
--- src/Product.js
+++ src/Product.js
@@ -2,23 +2,24 @@ import React from 'react';
 import './Product.css';
 import StarRateOutlinedIcon from '@material-ui/icons/StarRateOutlined';
 
-function Product() {
+function Product({ title, price, image, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
-          <StarRateOutlinedIcon />
+          {Array(rating)
+            .fill()
+            .map((star) => (
+              <StarRateOutlinedIcon key={star} />
+            ))}
         </div>
       </div>
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/71SGITPxbgL._AC_SL1500_.jpg"
-        alt=""
-      />
+      <img src={image} alt="" />
       <button>Add to basket</button>
     </div>
   );
